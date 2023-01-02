7df791b119bd410e8ba58663ebd83a2fa9f35355
Simon Mourier <simon_mourier@hotmail.com>
2023-01-02 09:38:56 +0100

    Updated year.

--- LuxaforTeamsCalling/Properties/AssemblyInfo.cs
+++ LuxaforTeamsCalling/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("LuxaforTeamsCalling")]
 [assembly: AssemblyProduct("Luxafor Teams Calling")]
 [assembly: AssemblyDescription("A Windows apps which lights up the Luxafor device when someone calls you on Microsoft Teams.")]
-[assembly: AssemblyCopyright("Copyright © 2017-2022 Simon Mourier. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2017-2023 Simon Mourier. All rights reserved.")]
 [assembly: ComVisible(false)]
 [assembly: Guid("36021203-980a-4da7-98c5-ef739e0187fd")]
 [assembly: AssemblyVersion("1.0.0.0")]
